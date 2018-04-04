e71a5e42f9f1fb3a929a31a909fded11a2c93998
mauro37ar <mauro31ar@gmail.com>
2018-04-03 21:28:46 -0300

    mariano y mauro ejercicio 1 c

--- practica1/ejercicio1a.py
+++ practica1/ejercicio1a.py
@@ -4,5 +4,5 @@ port = 22
 
 banner= "SSH Port"
 
-print("[+] checking for %s on port %s")% (banner, port)
-
+print("[+] Checking for %s on port %s" % (banner, port))
+print("[+] Checking for " + str(banner) +  " on port " + str(port))
---
+++ practica1/punto1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+port = 22
+banner = "SSH PORT"
+
+print "[+] checking for variable %s on port %i "%(banner,port)
+
+  
