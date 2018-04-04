6550f3aaae3c0629d108a242de07d0ed82260e29
Your Name <pablomatt>
2018-04-03 21:26:59 -0300

    subiendo Ejercicio 1e y 2e

---
+++ practica1/ejercicio1e.py
@@ -0,0 +1,9 @@
+numero = int(input("Ingrese un Numero "))
+div = numero/2
+modulo = div%2
+if modulo  == 0:
+	print ("El Numero Ingresado es par")
+else:
+	print ("El numero Ingresado es impar")
+
+
